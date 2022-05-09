55f8bef15ba90cd24699595dfa41708f9225aeb0
unknown <you@example.com>
2022-05-09 14:48:50 +0100

    website can load data from database and show it on popups using timeago and axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import * as React from "react";
-import Map, { Marker } from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { LocationOn } from "@material-ui/icons";
+import { LocationOn, Star } from "@material-ui/icons";
+import "./app.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
 
 function App() {
+    const [pins, setPins] = useState([]);
+
+    useEffect(() => {
+        const getPins = async () => {
+            try {
+                const res = await axios.get("/pins");
+                setPins(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getPins();
+    }, []);
+
     return (
         <Map
             initialViewState={{
@@ -15,13 +33,44 @@ function App() {
             mapboxAccessToken={process.env.REACT_APP_MAPBOX}
             mapStyle="mapbox://styles/mapbox/streets-v9"
         >
-            <Marker longitude={9.8642} latitude={37.2768} anchor="bottom">
-                <LocationOn
-                    style={{
-                        color: "slateblue",
-                    }}
-                />
-            </Marker>
+            {pins.map((p) => (
+                <>
+                    <Marker
+                        longitude={p.lat}
+                        latitude={p.long}
+                        offsetLeft={-20}
+                        offsetTop={-10}
+                    >
+                        <LocationOn
+                            style={{
+                                color: "slateblue",
+                            }}
+                        />
+                    </Marker>
+                    <Popup longitude={p.lat} latitude={p.long} anchor="left">
+                        <div className="card">
+                            <label>Ism l7anout</label>
+                            <h4 className="place"> {p.title}</h4>
+                            <label>chnowa chrit w b9adech</label>
+                            <p className="desc">{p.desc}</p>
+                            <label>a3tih note</label>
+                            <div className="stars">
+                                <Star className="stars" />
+                                <Star className="stars" />
+                                <Star className="stars" />
+                                <Star className="stars" />
+                                <Star className="stars" />
+                            </div>
+
+                            <label>Information</label>
+                            <span className="username">
+                                Created by <b>{p.username}</b>
+                            </span>
+                            <span className="date">{format(p.createdAt)}</span>
+                        </div>
+                    </Popup>
+                </>
+            ))}
         </Map>
     );
 }
